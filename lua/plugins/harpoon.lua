0fa9481ef39ad35269e3c3fa7f8124d8a0663081
prakhargupta-jan <2020kuec2007@iiitkota.ac.in>
2024-05-31 12:04:09 +0530

    setup Completions

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,7 +3,7 @@ return {
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function() 
+		config = function()
 			local harpoon = require("harpoon")
 			harpoon:setup()
 			vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -1,14 +1,20 @@
 return {
-	'nvimtools/none-ls.nvim',
-	config = function()
-		local null_ls = require 'null-ls'
-		null_ls.setup {
-			sources = {
-				-- lua
-				null_ls.builtins.formatting.stylua,
-				-- c++
-				null_ls.builtins.formatting.clang_format ,
-			},
-		}
-	end,
+  'nvimtools/none-ls.nvim',
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+  },
+  config = function()
+    local null_ls = require 'null-ls'
+    null_ls.setup {
+      sources = {
+        -- lua
+        null_ls.builtins.formatting.stylua,
+        -- c++
+        null_ls.builtins.formatting.clang_format,
+        -- ts/js
+        null_ls.builtins.formatting.prettier,
+		require("none-ls.diagnostics.eslint_d")
+      },
+    }
+  end,
 }
